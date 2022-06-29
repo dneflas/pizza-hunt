20f92837201dc510e944237beb571c2e03292250
Debbie Neflas <debbieneflas@gmail.com>
2022-06-29 13:07:41 -0700

    mongoose validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,9 +5,13 @@ const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: "Pizza name is required",
+      trim: true,
     },
     createdBy: {
       type: String,
+      required: "Username is required",
+      trim: true,
     },
     createdAt: {
       type: Date,
@@ -16,6 +20,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      require: true,
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
       default: "Large",
     },
     toppings: [],
